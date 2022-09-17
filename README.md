ff09512c43fe68ba9fef7ff8ddf2e94a74e143ff
oguzerdo <oe@eskisehir.edu.tr>
2022-09-17 20:05:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# FLO - CLTV Prediction
+
+![Untitled](FLO%20-%20CLTV%20Prediction%20691d1a9196f3499aaa37d825c6800c01/Untitled.png)
+
+# Business Problem
+
+[FLO](https://www.flo.com.tr/) is a large e-commerce site in Turkey.
+
+FLO wants to determine a roadmap for its sales and marketing activities.
+
+In order to plan for the medium to long term, the company needs to estimate the potential value that existing customers will provide to the company in the future.
+
+---
+
+# Dataset Info
+
+**Total Features:** 12
+
+**Total Row:** 19.945
+
+| Feature | Definition |
+| --- | --- |
+| master_id | Unique Customer Number |
+| order_channel | Which channel of the shopping platform is used (Android, IOS, Desktop, Mobile) |
+| last_order_channel | The channel where the most recent purchase was made |
+| first_order_date | Date of the customer's first purchase |
+| last_order_channel | Customer's previous shopping history |
+| last_order_date_offline | The date of the last purchase made by the customer on the offline platform |
+| order_num_total_ever_online | Total number of purchases made by the customer on the online platform |
+| order_num_total_ever_offline | Total number of purchases made by the customer on the offline platform |
+| customer_value_total_ever_offline | Total fees paid for the customer's offline purchases |
+| customer_value_total_ever_online | Total fees paid for the customer's online purchases |
+| interested_in_categories_12 | List of categories the customer has shopped in the last 12 months |
+
+---
+
+# Requirements
+
+```python
+Lifetimes==0.11.3
+pandas==1.4.3
+scikit_learn==1.1.2
+```
+
+---
+
+# Author
+
+[Oğuz Erdoğan](http://www.oguzerdogan.com)
---
+++ app.py
@@ -0,0 +1,95 @@
+# Libraries
+import pandas as pd
+import datetime as dt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from sklearn.preprocessing import MinMaxScaler
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = round(low_limit,0)
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = round(up_limit,0)
+
+
+def create_cltv_df(dataframe):
+
+    # Data Preparation
+    columns = ["order_num_total_ever_online", "order_num_total_ever_offline", "customer_value_total_ever_offline","customer_value_total_ever_online"]
+    for col in columns:
+        replace_with_thresholds(dataframe, col)
+
+    dataframe["order_num_total"] = dataframe["order_num_total_ever_online"] + dataframe["order_num_total_ever_offline"]
+    dataframe["customer_value_total"] = dataframe["customer_value_total_ever_offline"] + dataframe["customer_value_total_ever_online"]
+    dataframe = dataframe[~(dataframe["customer_value_total"] == 0) | (dataframe["order_num_total"] == 0)]
+    date_columns = dataframe.columns[dataframe.columns.str.contains("date")]
+    dataframe[date_columns] = dataframe[date_columns].apply(pd.to_datetime)
+
+    # Creating CLTV data structure
+    dataframe["last_order_date"].max()  # 2021-05-30
+    analysis_date = dt.datetime(2021, 6, 1)
+    cltv_df = pd.DataFrame()
+    cltv_df["customer_id"] = dataframe["master_id"]
+    cltv_df["recency_cltv_weekly"] = ((dataframe["last_order_date"] - dataframe["first_order_date"]).astype('timedelta64[D]')) / 7
+    cltv_df["T_weekly"] = ((analysis_date - dataframe["first_order_date"]).astype('timedelta64[D]')) / 7
+    cltv_df["frequency"] = dataframe["order_num_total"]
+    cltv_df["monetary_cltv_avg"] = dataframe["customer_value_total"] / dataframe["order_num_total"]
+    cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
+
+    # BG-NBD Model
+    bgf = BetaGeoFitter(penalizer_coef=0.001)
+    bgf.fit(cltv_df['frequency'],
+            cltv_df['recency_cltv_weekly'],
+            cltv_df['T_weekly'])
+    cltv_df["exp_sales_3_month"] = bgf.predict(4 * 3,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency_cltv_weekly'],
+                                               cltv_df['T_weekly'])
+    cltv_df["exp_sales_6_month"] = bgf.predict(4 * 6,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency_cltv_weekly'],
+                                               cltv_df['T_weekly'])
+
+    # # Gamma-Gamma Model
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(cltv_df['frequency'], cltv_df['monetary_cltv_avg'])
+    cltv_df["exp_average_value"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                           cltv_df['monetary_cltv_avg'])
+
+    # CLTV Prediction
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency_cltv_weekly'],
+                                       cltv_df['T_weekly'],
+                                       cltv_df['monetary_cltv_avg'],
+                                       time=6,
+                                       freq="W",
+                                       discount_rate=0.01)
+    cltv_df["cltv"] = cltv
+
+    # CLTV segmentation
+    cltv_df["cltv_segment"] = pd.qcut(cltv_df["cltv"], 4, labels=["D", "C", "B", "A"])
+
+    return cltv_df
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv('data/flo_data_20k.csv')
+    cltv_df = create_cltv_df(df)
+    print(cltv_df)
+    cltv_df.to_csv('output/cltv_segments.csv', index=False)
+
+
+
+
+
+
+
